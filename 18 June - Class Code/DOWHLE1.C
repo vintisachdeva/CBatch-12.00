de216dd175db00ecb9805dbb76fbbf51f22cfa8d
vintisachdeva <vintisachdeva17@gmail.com>
2018-06-18 14:13:12 +0530

    Add files via upload

---
+++ 18 June - Class Code/DOWHLE1.C
@@ -0,0 +1,13 @@
+void main(){
+	int num = 11;
+	clrscr();
+
+	do{
+		printf("%d\n", num);
+		num++;
+
+	}while(num<=10);
+
+	getch();
+
+}
---
+++ 18 June - Class Code/FOR1.C
@@ -0,0 +1,15 @@
+// init, cond, incre/decre
+
+void main(){
+	int num; // declare
+	clrscr();
+
+	for(num = 1; num<=10; num++){    // num = 11<=10
+	      printf("%d", num);  // 123..10
+
+	}
+	getch();
+
+
+
+}
